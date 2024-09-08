ef13139a8a5b35aef21bf8dc52381aca41d7f764
vishnu-2702 <vishnu.v2704@gmail.com>
2024-09-08 14:15:04 +0530

    new file:   .gitignore 	modified:   bin/Debug/net8.0/testproject.dll 	modified:   bin/Debug/net8.0/testproject.exe 	modified:   bin/Debug/net8.0/testproject.pdb 	modified:   obj/Debug/net8.0/apphost.exe 	modified:   obj/Debug/net8.0/ref/testproject.dll 	modified:   obj/Debug/net8.0/refint/testproject.dll 	modified:   obj/Debug/net8.0/testproject.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/testproject.AssemblyInfoInputs.cache 	modified:   obj/Debug/net8.0/testproject.csproj.FileListAbsolute.txt modified:   obj/Debug/net8.0/testproject.dll 	modified:   obj/Debug/net8.0/testproject.pdb

--- obj/Debug/net8.0/testproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/testproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce9108c1ba51eefe2a7a375a99bba7ee989bcf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d70aa95007e82faef494c9cd95687011a23817b")]
 [assembly: System.Reflection.AssemblyProductAttribute("testproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
