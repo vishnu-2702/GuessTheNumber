5ce9108c1ba51eefe2a7a375a99bba7ee989bcf1
vishnu-2702 <vishnu.v2704@gmail.com>
2024-09-08 13:49:22 +0530

    new file:   Program.cs 	new file:   bin/Debug/net8.0/testproject.deps.json 	new file:   bin/Debug/net8.0/testproject.dll 	new file:   bin/Debug/net8.0/testproject.exe 	new file:   bin/Debug/net8.0/testproject.pdb 	new file:   bin/Debug/net8.0/testproject.runtimeconfig.json 	new file:   obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   obj/Debug/net8.0/apphost.exe 	new file:   obj/Debug/net8.0/ref/testproject.dll 	new file:   obj/Debug/net8.0/refint/testproject.dll 	new file:   obj/Debug/net8.0/testproject.AssemblyInfo.cs 	new file:   obj/Debug/net8.0/testproject.AssemblyInfoInputs.cache 	new file:   obj/Debug/net8.0/testproject.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net8.0/testproject.GlobalUsings.g.cs 	new file:   obj/Debug/net8.0/testproject.assets.cache 	new file:   obj/Debug/net8.0/testproject.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net8.0/testproject.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net8.0/testproject.dll 	new file:   obj/Debug/net8.0/testproject.genruntimeconfig.cache 	new file:   obj/Debug/net8.0/testproject.pdb 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache 	new file:   obj/testproject.csproj.nuget.dgspec.json 	new file:   obj/testproject.csproj.nuget.g.props 	new file:   obj/testproject.csproj.nuget.g.targets 	new file:   testproject.csproj 	new file:   testproject.sln

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GuessTheNumber
+{
+  class GuessTheNumber
+  {
+
+    static string evaluateGuess(int Number, int Guess){
+        int a = Number;
+        int b = Guess;
+
+        if (a == b){
+            return "correct";
+        }
+        if (b < a ){
+            return "the guess is low";
+        }
+        if (b > a){
+            return "guess is high";
+        }
+        return "";
+    }
+
+    static void Main(string[] args)
+    {
+      Random random= new Random();
+      int number = random.Next(11);
+
+      Console.WriteLine("Guess the number i am thinking between 1 - 10");
+
+      int tries = 0;
+      string result =  "";
+      int guess;
+
+      while(result != "correct"){
+        guess = Convert.ToInt32(Console.ReadLine());
+        tries ++;
+        result = evaluateGuess(number, guess);
+        Console.WriteLine(result);
+
+        if (result == "correct"){
+            Console.WriteLine($"You have guessed it right in {tries} tries");
+        }
+      }
+      Console.ReadLine();
+    }
+  }
+}
